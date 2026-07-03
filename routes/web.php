09c56f8679343f5b29a54e4e8e0f8a02e332f29d
rsthopaz <thopazrosadi15@gmail.com>
2026-07-03 20:27:20 +0700

    Group + Challenge

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    $producturl = route('product.view', ['id'=>12]);
+    dd($producturl);
     return view('welcome');
 });
 
@@ -12,7 +15,8 @@ Route::get('/about', function () {
 
 Route::get('/producti/{id}', function(string $id){
     return "Product ID=$id";
-}) ->whereNumber("id")
+}) ->name("product.view")
+    ->whereNumber("id")
 ;
 
 Route::get('/user/{username}', function(string $username){
@@ -23,4 +27,18 @@ Route::get('/user/{username}', function(string $username){
 Route::get('{lang}/productc/{category?}', function(string $lang, string $category = null){
     return "Prodcut Category=$category from Lang=$lang";
 }) ->where(['lang' => '[a-z]{2}', 'category' => '[a-z]{4,}'])
+;
+
+Route::prefix('admin')->group(function (){
+    Route::get('/users', function(){
+        return 'admin/users';
+    });
+});
+
+// challenge
+Route::get('sum/{num1}/{num2}', function(string $num1, string $num2){
+    return $num1 + $num2;
+
+}) ->whereNumber("num1", "num2")
+
 ;
