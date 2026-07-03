101c52df5e25a84209bc6caba65102ba885a0dfc
rsthopaz <thopazrosadi15@gmail.com>
2026-07-03 19:20:50 +0700

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/producti/{id}', function(string $id){
+    return "Product ID=$id";
+});
+
+Route::get('/productc/{category?}', function(string $catogery = null){
+    return "Prodcut Category=$category";
+});
