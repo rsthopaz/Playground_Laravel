7e4b6c4f53d8d977952b011e5e690a72a83516d3
rsthopaz <thopazrosadi15@gmail.com>
2026-07-03 18:23:45 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>tes</h1>
+
+<p>Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library </p>
